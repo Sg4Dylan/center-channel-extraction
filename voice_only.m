3e2a4bf8547b6fd6b3ebebcb3ac36cf7c3096adc
Sg4Dylan <dengbill123@gmail.com>
2017-02-12 13:28:57 +0800

    Fixed bug with variable name, added support for R2014 and subsequent versions

--- voice_only.m
+++ voice_only.m
@@ -1,4 +1,7 @@
-song=wavread('input.wav');
+% NOTICE: At present this code can not be used at all.
+
+[song,sFs]=audioread('input.wav');
+% sound(song,sFs);
 left=song(:,1);
 right=song(:,2);
 N=length(left);
@@ -14,17 +17,18 @@ temp1=l+r;
 b=dot(c,temp1);
 c1=dot(l,r);
 %% solve the equation 
-coeff=[a,-b,s];  %coefficient matrix
+coeff=[a,-b,c1];  %coefficient matrix
 o=roots(coeff);  %solution
 a1=o(1); %% quadratic equation defined by coefficient  matrix
-newc=o1*c;
+newc=a1*c;
 newl=l-(a*l);
 newr=r-(a*r);
-ctime=ifft(newec);
+ctime=ifft(newc);
 ltime=ifft(newl);
 rtime=ifft(newr);
-sound((50*ctime),44100)  % amplify
-wavwrite(ctime,44100,'voiceonly.wav'); 
-wavwrite((0.0001*ltime),44100,'voiceonlyleft.wav')  %left channel
-wavwrite((0.0001*rtime),44100,'voiceonlyright.wav')  %right channel 
+% sound((50*ctime),44100)  % amplify
+audiowrite('voiceonly.wav',ctime,44100); 
+audiowrite('voiceonlyleft.wav',(0.0001*ltime),44100)  %left channel
+audiowrite('voiceonlyright.wav',(0.0001*rtime),44100)  %right channel 
 % Current issue is that it has lot of noise. If anyone interested to solve it can create a patch and create pull request. I will happyliy accept it.I am not working on this anymore
+% Completely full of noise
